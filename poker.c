a1c291e2f84b184dae3ae73b5cb7d8e6ab570ef5
SomeGuyWhoLovesProgramming <aidanbabu@live.co.uk>
2024-08-28 23:09:43 +0100

    got some networking working between 2 devices

--- poker.c
+++ poker.c
@@ -2,21 +2,55 @@
 
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <errno.h>
+#include <time.h>
 #include <sys/socket.h> 
+#include <netdb.h>
 #include <netinet/in.h> 
 #include <arpa/inet.h> 
 #include <string.h>
 #include <unistd.h>
 #include <poll.h>
 #include <errno.h>
+#include <math.h>
 
-int get_rank(unsigned char card) 
+void get_rank(unsigned char card, char* buffer) 
 {
-	return card & 15;
+	switch (card & 15) 
+	{
+		case 0:
+			strncpy(buffer, "A", 1);
+			break;
+		case 10:
+			strncpy(buffer, "J", 1);
+			break;
+		case 11:
+			strncpy(buffer, "Q", 1);
+			break;
+		case 12:
+			strncpy(buffer, "K", 1);
+			break;
+		default:
+			char digits[3];
+
+			sprintf(digits, "%d", (card & 15) + 1);
+
+			if (card & 15 < 9) 
+			{
+				strncpy(buffer, digits, 1);
+			}
+			else 
+			{
+				strncpy(buffer, digits, 2);
+			}
+			break;
+	}
 }
 
 void get_rank_upside_down(unsigned char card, char* buffer, int* len) 
 {
+	memset(buffer, 0, 8);
+	
 	const char* ace = "\U00002C6F\0";
 	const char* two = "\u218A\0";
 	const char* three = "\U0000218B\0";
@@ -71,19 +105,19 @@ void get_rank_upside_down(unsigned char card, char* buffer, int* len)
 			break;
 		case 9:
 			strcat(buffer, ten);
-			*len = 2;
+			*len = 3;
 			break;
 		case 10:
-			strcat(buffer,  jack);
-			*len = 3;
+			strcat(buffer, jack);
+			*len = 2;
 			break;
 		case 11:
 			strcat(buffer,  queen);
-			*len = 3;
+			*len = 2;
 			break;
 		case 12:
 			strcat(buffer,  king);
-			*len = 3;
+			*len = 2;
 			break;
 	}
 }
@@ -100,6 +134,32 @@ void get_suit(unsigned char card, char* buffer)
 	switch (card & 48) 
 	{
 		case 0:
+			strncpy(buffer, spade, 4);
+			break;
+		case 16:
+			strncpy(buffer, heart, 4);
+			break;
+		case 32:
+			strncpy(buffer, diamond, 4);
+			break;
+		case 48:
+			strncpy(buffer, club, 4);
+			break;
+	}
+}
+
+void get_suit_upside_down(unsigned char card, char* buffer) 
+{
+	memset(buffer, 0, 8);
+
+	const char* spade = "\U00002665\0";
+	const char* heart = " \U00002660\0";
+	const char* diamond = "\U00002666\0";
+	const char* club = "\U000025CF\0";
+
+	switch (card & 48) 
+	{
+		case 0:
 			strcat(buffer, spade);
 			break;
 		case 16:
@@ -111,7 +171,7 @@ void get_suit(unsigned char card, char* buffer)
 		case 48:
 			strcat(buffer, club);
 			break;
-	}
+	}	
 }
 
 void display_card(unsigned char card, char* buffer) 
@@ -137,42 +197,131 @@ void display_card(unsigned char card, char* buffer)
 	
 	buffer[140] = 0;
 
-	char rank[9];
+	char suit[5];
+	char rank[10];
+	char upside_down_rank[10];
 	int len;
 
-	get_rank_upside_down(9, rank, &len);
+	get_suit(card, suit);
+	get_rank(card, rank);
+	get_rank_upside_down(card, upside_down_rank, &len);
 
-	printf(
-		" ___________ \n"
-		"|           |\n"
-		"| 2         |\n"
-		"|           |\n"
-		"|           |\n"
-		"|     A     |\n"
-		"|           |\n"
-		"|           |\n"
-		"|         %s |\n"
-		"|___________|\n"
-	, rank);
-
-	printf("%s\n", buffer);
+	printf("%.30s", buffer);
+	printf("%s", rank);
+	printf("%.*s", 45 - len + 2, buffer + 31 + len - 2);
+	printf("%s", suit);
+	printf("%.*s", 45 - len + 2, buffer + 77);
+	printf("%s", upside_down_rank);
+	printf("%s\n", buffer + 123);
 }
 
-void host(int sfd) 
+void host() 
 {
-	listen(sfd, 10);
+	int socket_fd;
 
-	int number_of_clients;
+	struct addrinfo hints;
+	struct addrinfo *result, *rp;
 
-	while (number_of_clients < 0) 
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_protocol = 0; // don't really need to this, line 299
+
+	int success = getaddrinfo(NULL, "3500", &hints, &result);
+
+	if (success != 0) 
+	{
+		printf("%s\n", gai_strerror(success));
+		exit(-1);
+	}
+
+	printf("%d\n", success);
+
+	char ip_buffer[16];
+
+	for (rp = result; rp != NULL; rp = rp->ai_next) 
+	{
+		memset(ip_buffer, 0, 16);
+		inet_ntop(AF_INET, rp->ai_addr->sa_data, ip_buffer, 16);	
+
+		printf("IP: %s\n", ip_buffer);
+		
+		socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+		int yes = 1;
+
+		if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) 
+		{
+			printf("?: %d\n", errno);
+			close(socket_fd);
+			exit(1);
+		}
+
+		int bind_success = bind(socket_fd, rp->ai_addr, rp->ai_addrlen);
+
+		if (bind_success == -1) 
+		{
+			printf("%d\n", errno);
+			printf("?\n");
+			close(socket_fd);
+			exit(1);
+		}
+		else 
+		{
+			break;
+		}
+	}
+
+	freeaddrinfo(result);
+
+	int number_of_clients = -1;
+
+	char input_buffer[3];
+
+	while (number_of_clients < 1) 
 	{
 		printf("Number of clients: ");
 
-		scanf("%d", &number_of_clients);
+		char* read_success = fgets(input_buffer, sizeof(input_buffer), stdin);
+
+		if (read_success == NULL) 
+		{
+			printf("Not an valid number.\n");
+		}
+		else 
+		{
+			number_of_clients = atoi(read_success);
+		}
 	}
+
+	int* csfds = malloc(sizeof(int) * number_of_clients);
+
+	listen(socket_fd, 10);
+
+	int sum = 0;
+
+	for (int i = 0; i < number_of_clients; i++)
+	{
+		csfds[i] = accept(socket_fd, 0, 0);
+
+		int num;
+
+		recv(csfds[i], &num, sizeof(num), 0);
+
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+
+	int avg = sum / number_of_clients;
+
+	srandom(avg);
 
 	unsigned char cards[52];
 	char suit[5];
+	char card_buffer[141];
 
 	memset(cards, 0, 52);
 
@@ -183,18 +332,37 @@ void host(int sfd)
 			cards[i * 13 + j] = i << 4 | j;
 
 			unsigned char card = cards[i * 13 + j];
-
-			get_suit(card, suit);
-
-			printf("%d %s\n", get_rank(card), suit);
 		}
 	}
 
-	char card_buffer[141];
+	// Fisher-Yates algorithm
+	for (int i = 51; i != 0; i--) 
+	{
+		double num = (double)random() / (double)RAND_MAX;
+		double normalised_num = num * i;
+		int index = floor(normalised_num);
+		unsigned char _card = cards[i];
 
-	display_card(10, card_buffer);
+		// There's probably a better way of swapping like using XOR but I'm too lazy and the compiler probably got me
+		cards[i] = cards[index];
+		cards[index] = _card;
+	}
 
-	close(sfd);
+	for (int i = 0; i < 52; i++) 
+	{
+		unsigned char card = cards[i];
+
+		display_card(card, card_buffer);
+	}
+
+	for (int i = 0; i < number_of_clients; i++) 
+	{
+		close(csfds[i]);
+	}
+	
+	free(csfds);
+
+	close(socket_fd);
 }
 
 int main(int argc, char** argv) 
@@ -207,132 +375,7 @@ int main(int argc, char** argv)
 
 	int type = atoi(argv[1]);
 
-	struct sockaddr_in my_add = {
-		AF_INET,
-		0x50c3,
-		0,
-
-	};
-
-	char host_name[256];
-
-	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (socket_fd == -1) 
-	{
-		printf("huh couldn't even create a socket: %d\n", errno);
-		return -1;
-	}
-
-	if (bind(socket_fd, (const struct sockaddr*)&my_add, sizeof(my_add)) == -1 && errno != 98) 
-	{
-		printf("couldn't bind, errno: %d\n", errno);
-		return -1;
-	}
-
-	struct sockaddr result = {0};
-	int len = sizeof(result);
-
-	getsockname(socket_fd, &result, &len); 
-
-	char ip_buffer[256];
-
-	inet_ntop(AF_INET, &result.sa_data, ip_buffer, 256);
-
-	printf("Your IP address: %s\n", ip_buffer);
-
-	host(socket_fd);
-
-	/*int used_socket_fd;
-
-	switch (type) 
-	{
-		case 0:
-			listen(socket_fd, 10);
-			used_socket_fd = accept(socket_fd, 0, 0);
-
-			if (used_socket_fd == -1) 
-			{
-				printf("couldnt accept incoming connection, errno: %d\n", errno);
-				return -1;
-			}
-
-			break;
-		case 1:
-			if (connect(socket_fd, (const struct sockaddr*)&my_add, sizeof(my_add))) 
-			{
-				printf("couldnt connect, errno: %d\n", errno);
-				return -1;
-			}
-			used_socket_fd = socket_fd;
-			break;
-		default:
-			printf("type a valid number dumb dumb\n");
-			return -1;
-	}
-
-	struct pollfd fds[2] = {
-		{
-			0,
-			POLLIN,
-			0
-		},
-		{
-			used_socket_fd,
-			POLLIN,
-			0 
-		}
-	};
-
-	char buffer[64];
-	char peer_buffer[64];
-
-	int poll_result;
-
-	while (poll(fds, 2, 60000) != 0)
-	{
-		memset(buffer, 0, 64);
-
-		poll_result = poll(fds, 2, 10000);
-
-		if (fds[0].revents & POLLIN) 
-		{
-			int bytes_read = read(0, buffer, 63);
-
-			if (bytes_read <= 1) continue;
-
-			buffer[bytes_read - 1] = 0;
-			send(used_socket_fd, buffer, 63, 0);
-		}
-		else if  (fds[1].revents & POLLIN) 
-		{
-			recv(used_socket_fd, peer_buffer, 63, 0);
-
-			if (peer_buffer[0] == 46) continue;
-
-			printf("%s\n", peer_buffer);
-		}
-
-		if (strncmp(buffer, "quit", 4) == 0 || strncmp(peer_buffer, "quit", 4) == 0) 
-		{
-			printf("frrrrr\n");
-			break;
-		}
-	}
-
-	if (!poll_result) 
-	{
-		memset(buffer, 0, 64);
-
-		const char quit_msg[] = "quit\n";
-
-		strcat(buffer, quit_msg);
-
-		send(used_socket_fd, buffer, 64, 0);
-	}
-
-	close(used_socket_fd);
-	close(socket_fd);*/
+	host();
 	
 	return 0;
 }
---
+++ pokerbuild.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+clear && gcc -lm -o ../poker poker.c && ../poker 0
